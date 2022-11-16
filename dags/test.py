072831ba1a624a7ddfd5577fa5612b5784f17dcc
GStechschulte <stechschulteg@gmail.com>
2022-11-16 06:57:19 +0100

    update to python decorators

--- dags/test.py
+++ dags/test.py
@@ -1,8 +1,11 @@
 import datetime as dt
-from airflow import DAG
+import pandas as pd
+#from airflow import DAG
+#from airflow.operators.bash import BashOperator
+#from airflow.operators.python import PythonOperator
+from airflow.decorators import task, dag
 
-from airflow.operators.bash import BashOperator
-from airflow.operators.python import PythonOperator
+#from airflow.decorators.python import python_task
 
 default_args = {
     'owner': 'me',
@@ -12,19 +15,36 @@ default_args = {
 }
 
 
-with DAG('airflow_tutorial_v01',
-         default_args=default_args, 
-         schedule='0 * * * *',
-         ) as dag:
+# with DAG('airflow_tutorial_v01',
+#          default_args=default_args, 
+#          schedule='0 * * * *',
+#          ) as dag:
     
-    def print_world():
-        print('world')
+#     def print_world():
+#         print('world')
 
-    print_hello = BashOperator(task_id='print_hello', 
-                               bash_command='echo "hello"')
-    sleep = BashOperator(task_id='sleep',
-                         bash_command='sleep 5')
-    print_statement = PythonOperator(task_id='print_world',
-                                 python_callable=print_world)
+#     print_hello = BashOperator(task_id='print_hello', 
+#                                bash_command='echo "hello"')
+#     sleep = BashOperator(task_id='sleep',
+#                          bash_command='sleep 5')
+#     print_statement = PythonOperator(task_id='print_world',
+#                                  python_callable=print_world)
 
-print_hello >> sleep >> print_statement
+# print_hello >> sleep >> print_statement
+
+@dag('print_statements', default_args=default_args)
+def printing():
+
+    @task()
+    def _print_world():
+        print('hello world')
+    
+    @task()
+    def _print_hey():
+        print('hey gabriel')
+    
+    _print_world()
+    _print_hey()
+
+# no longer required to instantiate DAG into a global variable
+printing()
